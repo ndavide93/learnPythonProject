26e0f7f4d17849fd24a10b483a57788e6a7e49e3
ndavide93 <ndavide@outlook.it>
2022-03-14 22:57:32 +0100

    commit

--- 2.5.1.11.py
+++ 2.5.1.11.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 line1=295743861  # list(input('inserisci 9 cifre'))
 line2=431865927
 line3=876192543
@@ -9,7 +12,11 @@ line8=928671354
 line9=154938672
 line=[]
 for r in range(9):
-    line=input("inserisci 9 numeri da 1 a 9")
-    r=r+1
-    if line
-    
+    line=input("inserisci 9 numeri da 1 a 9 per la linea " + str(r+1) + ' : ')
+    if len(line) == 9 and line.isdigit() :
+        True
+    else:
+        print('riprova')
+        break
+def ctrl(linea):
+    if linea.sort
