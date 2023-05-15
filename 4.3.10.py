64abff4d1858c9150758f421e8752f5dc563af0d
ndavide93 <ndavide@outlook.it>
2023-05-15 22:59:09 +0200

    4.3.10 da rifare

--- 4.3.10.py
+++ 4.3.10.py
@@ -6,10 +6,22 @@ try:
     percorso=input('inserisci il percorso del file da aprire : ')
     file= open(percorso, 'rt')
     line = file.readline()
+    studenti_voti={}
     if line != '' :
+        nome=str()
+        voto=float()
+        for char in line:
+            if char.isalpha() or char == ' ':
+                nome=nome+char
+                continue
+            else:
+                studenti_voti[nome]=''
+                voto+=float(char)
+                studenti_voti[nome]=voto
+                continue
         
-        
-            
 
+        
+        
 except:
     print('non va')
