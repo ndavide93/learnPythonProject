6d9b1ac43a5a5e8b698615b8e9e2f39936633e81
ndavide93 <ndavide@outlook.it>
2023-11-20 21:57:17 +0100

    -

--- 4.6.13.py
+++ 4.6.13.py
@@ -1,20 +1,12 @@
-import calendar
-class MyCalendar(calendar.Calendar):
-    def count_weekday_in_year(self, year, weekday):
-        contagiorni = 0
-        for mese in range(1, 13):
-            for gg in calendar.Calendar.monthdays2calendar(self, year, mese):
-                for a,b in gg:
-                    if b == weekday and a != 0:
-                        contagiorni += 1
-        return contagiorni
-My_Calendar = MyCalendar()
-print(My_Calendar.count_weekday_in_year(year=2019, weekday=0))
+# file a.py
+print("a", end='')
 
+# file b.py
+import a
+print("b", end='')
 
+# file c.py
+print("c", end='')
+import a
+import b
 
-#dato l'anno e il giorno [0 lunedi]
-##fin tanto che il mese è >=12   --> for
-# per ogni giorno nel year , se weekday = 0 aumenta il contatore
-
- 
